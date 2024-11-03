86a5e2c13993bfcd923b100229066cdf5cd5f3fc
Gokul-anand-29 <gokulanand200@gmail.com>
2024-11-03 15:48:31 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -16,22 +16,13 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   int lowerLimit = 0;
   int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-    default: 
-      upperLimit = 40;
-      break;
+   if (coolingType == PASSIVE_COOLING) {
+        upperLimit = 35;
+    } else if (coolingType == HI_ACTIVE_COOLING) {
+        upperLimit = 45;
+    } else {
+        upperLimit = 40; // MED_ACTIVE_COOLING and default case
+    }
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
@@ -42,18 +33,16 @@ void sendToController(BreachType breachType) {
 
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
+  if (breachType == TOO_LOW) {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
+  }
+  else if (breachType == TOO_HIGH) {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too high\n");
-      break;
-    default:
-     // NORMAL
-      break;
+  }
+  else{
+    //DO Nothing
   }
 }
 
@@ -61,12 +50,9 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
 
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
+ if (alertTarget == TO_CONTROLLER) {
+        sendToController(breachType);
+    } else {
+        sendToEmail(breachType);
+    }
 }
